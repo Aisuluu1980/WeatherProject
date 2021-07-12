df166f211b1b3b7e01ed34d96947b177bd5a2ef0
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-07-13 02:11:48 +0600

    #48 Get Request(@Query, @Path).

--- app/src/main/java/kg/tutorialapp/weatherproject/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/weatherproject/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kg.tutorialapp.weatherproject.models.ForeCast
+import kg.tutorialapp.weatherproject.models.Post
+import kg.tutorialapp.weatherproject.network.PostsApi
 import kg.tutorialapp.weatherproject.network.WeatherApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -19,25 +21,120 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val call = weatherApi.getWeather()
-        call.enqueue(object: Callback<ForeCast> {
+        //fetchWeather()
+        //fetchWeatherUsingQuery()
+        //fetchPostById()
+        //createPost()
+        //createPostUsingFields()
+        createPostUsingFieldsMap()
+    }
+
+    private fun createPostUsingFieldsMap() {
+        val map = HashMap<String, String>().apply {
+            put("userId", "55")
+            put("title", "SUF!!!")
+            put("body", "Karakol")
+        }
+
+        val call = postsApi.createPostUsingFieldsMap(map)
+
+        call.enqueue(object: Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                val resultPost = response.body()
+                resultPost?.let {
+                    val resultText = "ID:" + it.id + "\n" +
+                            "userID:" + it.userId + "\n" +
+                            "TITLE:" + it.title + "\n" +
+                            "BODY:" + it.body + "\n"
+                    textView.text = resultText
+                }
+            }
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+            }
+        })
+    }
+
+    private fun createPostUsingFields() {
+        val call = postsApi.createPostUsingFields(userId = "99", title = "HI!!!", body = "Osh")
+        call.enqueue(object: Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                val resultPost = response.body()
+                resultPost?.let {
+                    val resultText = "ID:" + it.id + "\n" +
+                            "userID:" + it.userId + "\n" +
+                            "TITLE:" + it.title + "\n" +
+                            "BODY:" + it.body + "\n"
+                    textView.text = resultText
+                }
+            }
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+            }
+        })
+    }
+
+    private fun createPost() {
+        val post = Post(userId = 42, title = "Hello", body = "BISHKEK")
+        val call = postsApi.createPost(post)
+        call.enqueue(object: Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                val resultPost = response.body()
+                resultPost?.let {
+                    val resultText = "ID:" + it.id + "\n" +
+                            "userID:" + it.userId + "\n" +
+                            "TITLE:" + it.title + "\n" +
+                            "BODY:" + it.body + "\n"
+                    textView.text = resultText
+                }
+            }
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+            }
+        })
+    }
+
+    private fun fetchPostById() {
+        val call = postsApi.fetchPostById(10)
+        call.enqueue(object: Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                val post = response.body()
+                post?.let {
+                    val resultText = "ID:" + it.id + "\n" +
+                            "userID:" + it.userId + "\n" +
+                            "TITLE:" + it.title + "\n" +
+                            "BODY:" + it.body + "\n"
+                    textView.text = resultText
+                }
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+
+
+            }
+        })
+    }
+
+    private fun fetchWeatherUsingQuery() {
+        val call = weatherApi.fetchWeatherUsingQuery(lat = 40.513999, lon = 72.816098)
+        call.enqueue(object : Callback<ForeCast> {
+
+
             override fun onResponse(call: Call<ForeCast>, response: Response<ForeCast>) {
                 if (response.isSuccessful) {
                     val foreCast = response.body()
                     foreCast?.let {
                         textView.text = it.current?.weather!![0].description
-                        textView2.text = it.timezone
-                        Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_LONG).show()
+                        textView2.text = it.current?.temp.toString()
+
+//                        Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_LONG).show()
                     }
                 }
             }
+
             override fun onFailure(call: Call<ForeCast>, t: Throwable) {
                 Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
             }
-
-        }
-        )
+        })
     }
+
     private val okhttp by lazy {
         val interceptor = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
@@ -47,12 +144,16 @@ class MainActivity : AppCompatActivity() {
 //lazy нужен чтобы выдавался один и тот же результат
     private val retrofit by lazy {
         Retrofit.Builder()
-                .baseUrl("https://api.openweathermap.org/data/2.5/")
+//                .baseUrl("https://api.openweathermap.org/data/2.5/")
+                .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okhttp)
                 .build()
     }
     private val weatherApi by lazy {
         retrofit.create(WeatherApi::class.java)
+    }
+    private  val postsApi by lazy {
+        retrofit.create(PostsApi::class.java)
     }
 }
