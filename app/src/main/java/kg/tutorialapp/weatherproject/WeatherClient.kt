f8a2586f81cffe64db5eac94c3ff5f512eb82a2e
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-07-13 03:08:07 +0600

    #50 SingleTon design pattern.

---
+++ app/src/main/java/kg/tutorialapp/weatherproject/WeatherClient.kt
@@ -0,0 +1,26 @@
+package kg.tutorialapp.weatherproject
+
+import kg.tutorialapp.weatherproject.network.WeatherApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object WeatherClient {
+    private val okHttp by lazy {
+        val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create()).client(okHttp)
+            .build()
+    }
+
+    val weatherApi by lazy {
+        retrofit.create(WeatherApi::class.java)
+    }
+}
+
