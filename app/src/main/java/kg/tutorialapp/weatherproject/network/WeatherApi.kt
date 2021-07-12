f3d861fa0b46b213d65417dfcf75526e413dfc19
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-07-13 01:27:55 +0600

    #47 Get Request(Retrofit).

---
+++ app/src/main/java/kg/tutorialapp/weatherproject/network/WeatherApi.kt
@@ -0,0 +1,12 @@
+package kg.tutorialapp.weatherproject.network
+
+import kg.tutorialapp.weatherproject.models.ForeCast
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface WeatherApi {
+    @GET("onecall?lat=42.8746&lon=74.5698&exclude=minutely&appid=7888dd77c388dc656f9784de8954c8cb&lang=ru&units=metric")
+
+    fun getWeather(): Call<ForeCast>
+}
